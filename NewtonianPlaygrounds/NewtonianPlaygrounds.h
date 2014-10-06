744b1411bda2689972d190591ae44e073637b62b
DeWittmm <dewittmm@gmail.com>
2014-10-05 22:28:36 -0700

    Initial Commit

---
+++ NewtonianPlaygrounds/NewtonianPlaygrounds.h
@@ -0,0 +1,19 @@
+//
+//  NewtonianPlaygrounds.h
+//  NewtonianPlaygrounds
+//
+//  Created by Michael DeWitt on 10/5/14.
+//  Copyright (c) 2014 DeWitt.PHYSICS.Playgrounds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewtonianPlaygrounds.
+FOUNDATION_EXPORT double NewtonianPlaygroundsVersionNumber;
+
+//! Project version string for NewtonianPlaygrounds.
+FOUNDATION_EXPORT const unsigned char NewtonianPlaygroundsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewtonianPlaygrounds/PublicHeader.h>
+
+
